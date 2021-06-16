d168684c49cb9ef8ae0dee9ac15ccab8f0e6734b
Mike Honey <mike.honey@mangasolutions.com>
2021-06-17 09:17:17 +1000

    Add files via upload

--- covid-19-genomes-get-outbreak-info.py
+++ covid-19-genomes-get-outbreak-info.py
@@ -48,7 +48,7 @@ def main():
     """
     print (str(datetime.datetime.now()) + ' Starting ...')
     webpageURL = 'https://outbreak.info/location-reports?loc=ZZZ&selected=S%3AE484K&selected=AV.1&selected=AY.1&selected=B.1.1.7&selected=B.1.1.7%20%2B%20S%3AE484K&selected=B.1.351&selected=B.1.617.1&selected=B.1.617.2&selected=B.1.1.318&selected=B.1.427&selected=B.1.429&selected=B.1.525&selected=B.1.526&selected=B.1.526.1&selected=B.1.526.2&selected=B.1.617&selected=B.1.617.1&selected=B.1.617.2&selected=B.1.617.3&selected=C.36.3&selected=C.37&selected=P.1&selected=P.1.1&selected=P.1.2&selected=P.2&selected=P.3'
-    webpageCountries = ['AUS', 'CAN', 'GBR', 'IDN', 'ISR', 'NZL', 'PER', 'SGP', 'USA', 'ZAF']
+    webpageCountries = ['AUS', 'CAN', 'GBR', 'IDN', 'ISR', 'JPN', 'NZL', 'PER', 'SGP', 'USA', 'ZAF']
     datadir = 'C:/Dev/covid-19-genomes/'
 
     chromeOptions = webdriver.ChromeOptions()
