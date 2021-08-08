01b2543423d93bb1fab6f3698eb394f13d830ef4
Mike Honey <mike.honey@mangasolutions.com>
2021-08-08 12:06:40 +1000

    Add files via upload

--- covid-19-genomes-get-nextstrain.py
+++ covid-19-genomes-get-nextstrain.py
@@ -1,12 +1,17 @@
 import datetime
+import gzip
+import json
 import numpy
 import os
 import pandas
 from pathlib import Path
-import requests
 import pytest
+import requests
+import shutil
 import time
-import json
+import urllib.request
+import zipfile
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
@@ -14,7 +19,6 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-import zipfile
 
 def FindElem(driver: webdriver, driver_by, selector: str, Timeout: int = 300):
     while Timeout > 0:
@@ -62,7 +66,10 @@ def main():
     driver = webdriver.Chrome(executable_path=chromedriver, chrome_options=chromeOptions)
 
     datadir = 'C:/Dev/covid-19-genomes/'
-
+    ncov_open_filename_out = 'nextstrain_ncov_open_metadata.tsv'
+    for p in Path(datadir).glob(ncov_open_filename_out):
+        p.unlink()
+ 
     webpageURL = 'https://nextstrain.org/'
     webpagePaths = ['community/aicbu/ncov/srilanka',
                     'community/alghoribi-lab/ncov/gcc',
@@ -73,43 +80,13 @@ def main():
                     # 'community/quipupe/Nextstrain_Chile', 
                     'community/quipupe/Nextstrain_Ecuador', 
                     'community/quipupe/Nextstrain_Peru', 
-                    # 'groups/neherlab/ncov/austria',
-                    # 'groups/neherlab/ncov/belarus',
-                    # 'groups/neherlab/ncov/belgium',
-                    # 'groups/neherlab/ncov/bosnia-and-herzegovina',
-                    # 'groups/neherlab/ncov/bulgaria',
-                    # 'groups/neherlab/ncov/czech-republic',
-                    # 'groups/neherlab/ncov/denmark',
-                    # 'groups/neherlab/ncov/estonia',
-                    # 'groups/neherlab/ncov/finland',
-                    # 'groups/neherlab/ncov/france',
-                    # 'groups/neherlab/ncov/germany',
-                    # 'groups/neherlab/ncov/greece',
-                    # 'groups/neherlab/ncov/hungary',
-                    # 'groups/neherlab/ncov/iceland',
-                    # 'groups/neherlab/ncov/india',
-                    # 'groups/neherlab/ncov/ireland',
-                    # 'groups/neherlab/ncov/israel',
-                    # 'groups/neherlab/ncov/italy',
-                    # 'groups/neherlab/ncov/latvia',
-                    # 'groups/neherlab/ncov/lithuania',
-                    # 'groups/neherlab/ncov/luxembourg',
-                    # 'groups/neherlab/ncov/netherlands',
-                    # 'groups/neherlab/ncov/norway',
-                    # 'groups/neherlab/ncov/poland',
-                    # 'groups/neherlab/ncov/portugal',
-                    # 'groups/neherlab/ncov/romania',
-                    # 'groups/neherlab/ncov/russia',
-                    # 'groups/neherlab/ncov/serbia',
-                    # 'groups/neherlab/ncov/slovakia',
-                    # 'groups/neherlab/ncov/slovenia',
-                    # 'groups/neherlab/ncov/spain',
-                    # 'groups/neherlab/ncov/sweden',
-                    # 'groups/neherlab/ncov/switzerland',
-                    # 'groups/neherlab/ncov/turkey',
-                    # 'groups/neherlab/ncov/united-kingdom',
-                    'groups/spheres/ncov/USA',
-                    # 'ncov/africa', 'ncov/asia', 'ncov/europe' , 'ncov/north-america', 'ncov/oceania', 'ncov/south-america'
+                    'groups/spheres/ncov/USA'
+                    # 'ncov/open/africa', 
+                    # 'ncov/open/asia', 
+                    # 'ncov/open/europe' , 
+                    # 'ncov/open/north-america', 
+                    # 'ncov/open/oceania', 
+                    # 'ncov/open/south-america'
                     ]
     # webpagePaths = ['groups/spheres/ncov/USA']
     metadata_name = "Metadata (TSV)"
@@ -170,6 +147,7 @@ def main():
                     'groups/neherlab/ncov/switzerland',
                     'groups/neherlab/ncov/turkey',
                     'groups/neherlab/ncov/united-kingdom',
+                    # 'groups/swiss/ncov/switzerland/?f_country=Switzerland',
                     'ncov/gisaid/africa', 
                     'ncov/gisaid/asia', 
                     'ncov/gisaid/europe' , 
@@ -184,7 +162,7 @@ def main():
         processWebPage(webpageURL, datadir, driver, eachPath, metadata_name)
 
     driver.quit()
-    print (str(datetime.datetime.now()) + ' Downloads complete, zipping results ...')
+    print (str(datetime.datetime.now()) + ' individual downloads complete, zipping results ...')
 
     zipfilename = 'nextstrain_ncov_metadata.zip'
     for p in Path(datadir).glob(zipfilename):
@@ -197,6 +175,16 @@ def main():
                         os.path.relpath(os.path.join(root, file), 
                                         os.path.join(datadir, '..')))
     zipf.close()
+
+    print (str(datetime.datetime.now()) + ' Processing ncov/open')
+    webpageURL = 'https://data.nextstrain.org/files/ncov/open/metadata.tsv.gz'
+    filename = 'metadata.tsv'
+    with urllib.request.urlopen(webpageURL) as response, open(datadir + filename + '.gz', 'wb') as out_file:
+        shutil.copyfileobj(response, out_file)
+
+    with gzip.open(datadir + filename + '.gz', 'rb') as f_in:
+        with open(datadir + ncov_open_filename_out, 'wb') as f_out:
+            shutil.copyfileobj(f_in, f_out)
 
     print (str(datetime.datetime.now()) + ' Finished!')
     exit()
