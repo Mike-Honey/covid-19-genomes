b6f99d5f714377bd0dbe12afbc5006e4121a6211
Mike Honey <mike.honey@mangasolutions.com>
2023-01-04 00:53:24 +1100

    Add files via upload

--- nextclade-refresh.py
+++ nextclade-refresh.py
@@ -1,6 +1,7 @@
 import datetime
 import os
 import subprocess
+import pandas
 
 
 def main():
@@ -13,7 +14,8 @@ def main():
         datadir + 'data\sars-cov-2 -q --output-tsv=' + \
         datadir + 'output\[filename].tsv ' + \
         datadir + 'input\[filename].fasta'
-    
+    needed_columns = ['seqName', 'clade', 'Nextclade_pango', 'partiallyAliased']
+
     for file in os.listdir(datadir + 'input'):
         filename = os.fsdecode(file)
         # process each input .fasta file, only if a matching output .tsv file does not exist.
@@ -21,6 +23,11 @@ def main():
             nextclade_cmd = str.replace(nextclade_cmd_template, "[filename]", str.replace(filename,".fasta", ""))
             print (str(datetime.datetime.now()) + ' Calling: ' + nextclade_cmd)
             subprocess.call (nextclade_cmd, shell=True)
+            
+            filename_tsv = str.replace(filename,".fasta", ".tsv")
+            print (str(datetime.datetime.now()) + ' Dropping columns from: ' + filename_tsv)
+            df1 = pandas.read_csv(datadir + "output/" + filename_tsv, usecols = needed_columns, sep="\t")
+            df1.to_csv(datadir + "output/" + filename_tsv, index=False, sep="\t" )
 
     print (str(datetime.datetime.now()) + ' Finished!')
     exit()
